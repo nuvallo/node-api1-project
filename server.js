a4c7cde95a1debd243ec489f93466dd2070acc83
nuvallo <nuvallo.inc@gmail.com>
2020-04-08 21:28:20 -0400

    database created

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-const express = require('express');
+const express = require("express");
+const db = require("./database");
 
-const hostName = '21.0.0.1';
+const hostName = "21.0.0.1";
 const PORT = 5000;
 
+const server = express();
 
-const server = express()
-
-server.get('/', (req, res)  => {
-    res.json({msg: 'Hello from server.js'})
+server.get("/", (req, res) => {
+  res.json({ msg: "Hello from server.js" });
 });
 
-
-
-
 server.listen(PORT, () => {
-    console.log(`Server started on ${hostName}:${PORT}`)
-})
+  console.log(`Server started on ${hostName}:${PORT}`);
+});
