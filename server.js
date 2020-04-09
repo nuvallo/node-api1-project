a3d7c64ee82d318e9891bc9a21301f95aff33790
nuvallo <nuvallo.inc@gmail.com>
2020-04-09 16:38:20 -0400

    POST request for new user finished

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = 5000;
 
 const server = express();
 
+server.use(express.json());
+
 // GET requests
 server.get("/", (req, res) => {
   res.json({ msg: "Welcome to NODE API PROJECT" });
@@ -19,20 +21,29 @@ server.get("/api/users/", (req, res) => {
 
 server.get("/api/users/:id", (req, res) => {
   const userId = req.params.id;
-  console.log(userId);
   const user = db.getUserById(userId);
 
   if (user) {
     res.json(user);
   } else {
-    res.status(400).json({ msg: "User not found" });
+    res.status(400).json({ errorMessage: "User not found" });
   }
 });
 
 // POST requests
-server.post("/api/users/:id", (req, res) => {
-  const userId = req.params.id;
-  const user = db.getUserById(userId);
+server.post("/api/users/", (req, res) => {
+  const newUser = db.createUser({
+    name: req.body.name,
+    bio: req.body.bio,
+  });
+
+  if (!req.body.name) {
+    return res.status(400).json({ errorMessage: "User needs a name" });
+  } else if (!req.body.bio) {
+    return res.status(400).json({ errorMessage: "User needs a bio" });
+  } else {
+    res.status(201).json(newUser);
+  }
 });
 
 // Listen Event
