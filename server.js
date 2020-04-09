baa375401435cb9f696cf40830300f77f2258b7d
nuvallo <nuvallo.inc@gmail.com>
2020-04-08 21:14:23 -0400

    express setup and server created

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 const express = require('express');
 
+const hostName = '21.0.0.1';
+const PORT = 5000;
+
+
+const server = express()
+
+server.get('/', (req, res)  => {
+    res.json({msg: 'Hello from server.js'})
+});
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`Server started on ${hostName}:${PORT}`)
+})
